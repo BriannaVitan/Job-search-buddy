215483cc5d821f3e3b2c224454c08a0dc478d2d6
BriannaVitan <briannavitan@gmail.com>
2024-11-08 19:55:46 -0600

    changes to background color and navbar in CSS

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = () => {
   }, [loginCheck]);  // Dependency array ensures useEffect runs when loginCheck changes
 
   return (
-    <div className="display-flex justify-space-between align-center py-2 px-5 mint-green">
+    <div className="div">
       <h1>
        Job Search Buddy 
       </h1>
